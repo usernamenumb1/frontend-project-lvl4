97b2c9845fa9342eb998ca236500e98ad80526be
ivanleonov <treska1315@gmail.com>
2022-06-16 22:25:17 +0300

    dropdown added

--- src/components/channels/Channel.jsx
+++ src/components/channels/Channel.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import cn from 'classnames';
 import { GrChannel } from 'react-icons/gr';
+import { BsChevronDown } from "react-icons/bs";
+import { Dropdown, Button, ButtonGroup } from "react-bootstrap";
 import { setId } from "../../slices/channelsSlice.js";
 
 export default ({ channel: { name, id } }) => {
@@ -11,13 +13,23 @@ export default ({ channel: { name, id } }) => {
     dispatch(setId(id));
   };
   return (
-    <button
-      type="button"
-      className={cn('w-100 rounded-1 text-start btn', { 'btn-secondary': id === currentChannelId })}
-      onClick={handleClick}
-    >
-      <GrChannel className="me-2 mb-1" />
-      {name}
-    </button>
+    <Dropdown as={ButtonGroup} className="d-flex pb-1">
+      <Button
+        variant={cn(' felx-grow-0 w-100 text-start btn', { 'btn-cadetblue-nohover': id === currentChannelId, 'btn-nocolor': id !== currentChannelId })}
+        onClick={handleClick}
+      >
+        <GrChannel className="me-2 mb-1" />
+        {name}
+      </Button>
+
+      <Dropdown.Toggle split variant={cn('btn', { 'btn-cadetblue': id === currentChannelId, 'btn-nocolor': id !== currentChannelId })} id="dropdown-split-basic">
+        <BsChevronDown />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Delete</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Rename</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
