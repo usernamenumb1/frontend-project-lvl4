0bc64c06f044c826812e16d2788a31f646a3f81e
ivanleonov <treska1315@gmail.com>
2022-06-17 15:17:41 +0300

    start adding modals

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import channelsReducer from "./channelsSlice.js";
 import massegesReducer from "./messagesSlice.js";
+import modalSlice from "./modalSlice.js";
 
 export default configureStore({
   reducer: {
     channelsStore: channelsReducer,
     messagesStore: massegesReducer,
+    modalStore: modalSlice,
   },
 });
---
+++ src/slices/modalSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isOpen: false,
+  type: null,
+  extraData: null,
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    setIsOpen: (state, { payload: { type, isOpen, extraData } }) => {
+      state.isOpen = isOpen;
+      state.type = type;
+      if (extraData) state.extraData = extraData;
+    },
+  },
+});
+
+export const { setIsOpen } = modalSlice.actions;
+export default modalSlice.reducer;
