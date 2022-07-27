de11c7ea88cb614c9b110ad6c020754f05408b75
ivanleonov <treska1315@gmail.com>
2022-07-27 22:19:11 +0300

    promissefied socket

--- src/components/context/APIProvider.jsx
+++ src/components/context/APIProvider.jsx
@@ -1,37 +1,42 @@
-import React, { createContext } from "react";
-import { useDispatch } from "react-redux";
-import { updateMessages } from "../../slices/messagesSlice.js";
-import { updateChannels, removeChannel, renameChannel } from "../../slices/channelsSlice.js";
+import React, { createContext, useState } from "react";
 
 export const apiContext = createContext();
 
 export const APIProvider = ({ children, socket }) => {
-  const dispatch = useDispatch();
-  const emitSendMessage = (mess) => socket.emit('newMessage', mess, (ack) => console.log(ack.status));
-  const emitAddNewChannel = (channel) => socket.emit('newChannel', channel);
-  const emitRemoveChannel = (id) => socket.emit('removeChannel', id);
-  const emitRenameChannel = (id, name) => socket.emit('renameChannel', { id, name });
-  socket.on("newMessage", (message) => {
-    dispatch(updateMessages(message));
-    console.log(message);
-  });
-  socket.on("newChannel", (channel) => {
-    dispatch(updateChannels(channel));
-    console.log(channel);
-  });
-  socket.on("removeChannel", ({ id }) => {
-    dispatch(removeChannel(id));
-    console.log(id);
-  });
-  socket.on("renameChannel", ({ id, name }) => {
-    console.log(name);
-    dispatch(renameChannel({ id, name }));
-  });
+  const [state, setState] = useState('pending');
+  const promisedEmit = (emitFunc) => (data) => new Promise((resolve, reject) => {
+    // let state = 'pending';
+    // setState('pending');
+    const timer = setTimeout(() => {
+      // state = 'rejected';
+      setState('rejected');
+      reject(new Error('something goes wrong'));
+    }, 3000);
+    console.log(state);
+    if (state !== 'pending') return;
+    emitFunc(data, (response) => {
+      clearTimeout(timer);
+      if (state !== 'pending') return;
+      console.log(state);
+      if (response.status === 'ok') {
+        // state = 'resolved';
+        setState('pending');
+        resolve(response);
+      } else {
+        setState('rejected');
+        reject(new Error('not ok'));
+      }
+      // state = 'rejected';
+    });
+  }).then((response) => console.log(response));
+  const socketAPI = {
+    sendMessage: promisedEmit((...arg) => socket.volatile.emit('newMessage', ...arg)),
+    addNewChannel: promisedEmit((...arg) => socket.volatile.emit('newChannel', ...arg)),
+    removeChannel: promisedEmit((...arg) => socket.volatile.emit('removeChannel', ...arg)),
+    renameChannel: promisedEmit((...arg) => socket.volatile.emit('renameChannel', ...arg)),
+  };
   return (
-    <apiContext.Provider value={{
-      emitRenameChannel, emitRemoveChannel, emitAddNewChannel, emitSendMessage, socket,
-    }}
-    >
+    <apiContext.Provider value={socketAPI}>
       {children}
     </apiContext.Provider>
   );
